d629a2655910c0401234ef88e8ff4586cbff51e6
33sametsen <70416969+33sametsen@users.noreply.github.com>
2020-12-15 00:50:19 +0300

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -13,7 +13,7 @@ async def ytdl(_, message):
     try:
         if userLastDownloadTime > datetime.now():
             wait_time = round((userLastDownloadTime - datetime.now()).total_seconds() / 60, 2)
-            await message.reply_text(f"`Wait {wait_time} Minutes before next Request`")
+            await message.reply_text(f"`Bekle {wait_time} Önceki istekten bir süre sonra ..`")
             return
     except:
         pass
@@ -28,10 +28,10 @@ async def ytdl(_, message):
                                      timedelta(minutes=youtube_next_fetch)
 
     except Exception:
-        await message.reply_text("`Failed To Fetch Youtube Data... 😔 \nPossible Youtube Blocked server ip \n#error`")
+        await message.reply_text("`Youtube Verileri Getirilemedi... 😔 \nOlası Youtube Engellenen sunucu IP'si \n#error`")
         return
     buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
-    sentm = await message.reply_text("Processing Youtube Url 🔎 🔎 🔎")
+    sentm = await message.reply_text("Youtube URL'si işleniyor 🔎 🔎 🔎")
     try:
         # Todo add webp image support in thumbnail by default not supported by pyrogram
         # https://www.youtube.com/watch?v=lTTajzrSkCw
